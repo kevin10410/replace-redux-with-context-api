01901b96310c876af766ca7cf97a449757e43496
OTree <kevin10410@gmail.com>
2020-03-09 20:34:29 +0800

    refactor(Products): replace useSelector with useContext

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from 'react';
 
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
+import { ProductsContext } from '../context/ContextProducts';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  const { products } = useContext(ProductsContext);
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {products.map(prod => (
         <ProductItem
           key={prod.id}
           id={prod.id}
